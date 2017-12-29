2cd2c3d112f1b7e81f39df5ee97f2233ac297e4a
phamhongphuc <phamhongphuc.info@gmail.com>
2017-12-29 10:53:26 +0700

    Button is LinearLayoutCompat

--- app/src/main/java/components/Button.java
+++ app/src/main/java/components/Button.java
@@ -4,58 +4,58 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.AttributeSet;
 
 import uit.group.manager.R;
 
-public class Button extends android.support.v7.widget.AppCompatButton {
+public class Button extends LinearLayoutCompat {
     private boolean mActive = false;
 
     public Button(@NonNull Context context) {
         super(context, null, R.attr.ButtonStyle);
-        init(context, null, null);
+        Initialize(context, null, null);
     }
 
     public Button(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs, R.attr.ButtonStyle);
-        init(context, attrs, null);
+        Initialize(context, attrs, null);
     }
 
     public Button(@NonNull Context context, String icon) {
         super(context, null, R.attr.ButtonStyle);
-        init(context, null, icon);
+        Initialize(context, null, icon);
     }
 
     public Button(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, R.attr.ButtonStyle);
-        init(context, attrs, null);
+        Initialize(context, attrs, null);
     }
 
-    private void init(@NonNull Context context, @Nullable AttributeSet attrs, @Nullable String icon) {
-        this.setTypeface(
-                Typeface.createFromAsset(getContext().getAssets(), "fonts/aicon.ttf")
-        );
-        this.setText(
-                attrs != null ?
-                        context.obtainStyledAttributes(attrs, R.styleable.Button).getText(R.styleable.Button__icon) :
-                        icon
-        );
+    private void Initialize(@NonNull Context context, @Nullable AttributeSet attrs, @Nullable String icon) {
+//        this.setTypeface(
+//                Typeface.createFromAsset(getContext().getAssets(), "fonts/aicon.ttf")
+//        );
+//        this.setText(
+//                attrs != null ?
+//                        context.obtainStyledAttributes(attrs, R.styleable.Button).getText(R.styleable.Button__icon) :
+//                        icon
+//        );
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int size = Math.min(
-                MeasureSpec.getSize(widthMeasureSpec),
-                MeasureSpec.getSize(heightMeasureSpec)
-        );
+//        int size = Math.min(
+//                MeasureSpec.getSize(widthMeasureSpec),
+//                MeasureSpec.getSize(heightMeasureSpec)
+//        );
 
         // setTextSize(26);
-        setTextSize(size * 0.4f / getResources().getDisplayMetrics().density);
-        setMeasuredDimension(size, size);
+//        setTextSize(size * 0.4f / getResources().getDisplayMetrics().density);
+//        setMeasuredDimension(size, size);
     }
 
     public void setActive() {
