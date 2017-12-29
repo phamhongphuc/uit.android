7b101318573fffc54fc19164d31b7c7e9f36dcc4
phamhongphuc <phamhongphuc.info@gmail.com>
2017-12-29 18:59:05 +0700

    New Project Activity

--- app/src/main/java/uit/group/manager/NewProjectActivity.java
+++ app/src/main/java/uit/group/manager/NewProjectActivity.java
@@ -1,14 +1,13 @@
 package uit.group.manager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class NewProjectActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, NewProjectActivity.class));
-        finish();
+        setContentView(R.layout.activity_new_project);
     }
 }
