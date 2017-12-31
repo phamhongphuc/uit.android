68e6f89b75858ad6a7b751e2f6f2f819b9599cec
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-31 19:41:12 +0700

    Add UserProfileActivity

---
+++ app/src/main/java/uit/group/manager/UserProfileActivity.java
@@ -0,0 +1,44 @@
+package uit.group.manager;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import view.fragment.FragmentAdapter;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+
+    private void InitializePages() {
+        ViewPager.OnPageChangeListener listener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+//                state.viewPagerIndex.set(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        };
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), new int[]{
+                R.layout.fragment_tasks,
+                R.layout.fragment_profile,
+        });
+        ViewPager viewPager = findViewById(R.id.viewPagerLogin);
+        viewPager.addOnPageChangeListener(listener);
+        viewPager.setAdapter(fragmentAdapter);
+    }
+
+}
