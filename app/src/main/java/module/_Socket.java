8e6e38d4723c8ab4525f20655160c2142459273d
Huyền <henrynguyen02081997@gmail.com>
2018-01-01 20:22:14 +0700

    Update socket (Finish)

--- app/src/main/java/module/_Socket.java
+++ app/src/main/java/module/_Socket.java
@@ -20,6 +20,7 @@ import io.socket.client.Ack;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import object.Channel;
 import object.Project;
 import object.Task;
 import object.User;
@@ -157,13 +158,14 @@ public class _Socket {
                     JSONObject obj = (JSONObject) args[1];
                     realm.beginTransaction();
                     final Project objectFromJson = realm.createOrUpdateObjectFromJson(Project.class, obj);
+                    /// Sau khi
                     realm.commitTransaction();
                 }
             }
         });
     }
 
-    public static void EditProject(final Project project) {
+    public static Project EditProject(final Project project) {
         socket.emit("Edit:Project(project)", project, new Ack() {
             @Override
             public void call(Object... args) {
@@ -174,16 +176,41 @@ public class _Socket {
                     JSONObject obj = (JSONObject) args[1];
                     realm.beginTransaction();
                     final Project objectFromJson = realm.createOrUpdateObjectFromJson(Project.class, obj);
+                    /// Sau khi chinh sua project
                     realm.commitTransaction();
                 }
             }
         });
+        return new Project();
     }
 
-    public static RealmList<Task> GetTasksByProjectId(String projectId) {
-        socket.emit("Get:Project.Tasks(projectId)", projectId, new Ack() {
+    public static Project CreateProject(final String userId) {
+        socket.emit("Create:Project(userId)", userId, new Ack() {
             @Override
             public void call(Object... args) {
+                Realm realm = Realm.getDefaultInstance();
+                if (args[0] != null) {
+                    Log.d("SOCKET: ERROR", "Lỗi thêm mới: " + args[0]);
+                } else {
+                    realm.beginTransaction();
+                    final Project newProject = realm
+                            .createOrUpdateObjectFromJson(Project
+                                    .class, (JSONObject) args[1]);
+                    /// sau khi da update xong, tra ve newProject
+                    realm.commitTransaction();
+                }
+            }
+        });
+        return new Project();
+    }
+
+    public static RealmList<Task> GetTasksByProjectId(final String projectId) {
+        socket.emit("Get:Project.Tasks(projectId)", projectId, new Ack() {
+            RealmList<Task> tasks = new RealmList<>();
+
+            @Override
+            public void call(Object... args) {
+                Realm realm = Realm.getDefaultInstance();
                 if (args[0] != null) {
                     Log.d("SOCKET: ERROR", "Lỗi trả về: " + args[0]);
                 } else {
@@ -191,6 +218,61 @@ public class _Socket {
                     for (int i = 0; i < array.length(); i++) {
                         try {
                             int taskId = (int) array.get(i);
+                            tasks.add(realm.where(Task.class).equalTo("id", taskId).findFirst());
+                            /// Sau khi da lay duoc ListTask
+                            /// cần return tasks
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        return new RealmList<>();
+    }
+
+    public static RealmList<Channel> GetChannelsByProjectId(final String projectId) {
+        socket.emit("Get:Project.Channels(projectId)", projectId, new Ack() {
+            RealmList<Channel> channels = new RealmList<>();
+
+            @Override
+            public void call(Object... args) {
+                Realm realm = Realm.getDefaultInstance();
+                if (args[0] != null) {
+                    Log.d("SOCKET: ERROR", "Lỗi trả về: " + args[0]);
+                } else {
+                    JSONArray array = (JSONArray) args[1];
+                    for (int i = 0; i < array.length(); i++) {
+                        try {
+                            String channelId = (String) array.get(i);
+                            channels.add(realm.where(Channel.class).equalTo("id", channelId).findFirst());
+                            /// cần return channels
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        return new RealmList<>();
+    }
+
+    public static RealmList<User> GetMembersByProjectId(final int projectId) {
+        socket.emit("Get:Project.Members(projectId)", new Ack() {
+            RealmList<User> users = new RealmList<>();
+
+            @Override
+            public void call(Object... args) {
+                Realm realm = Realm.getDefaultInstance();
+                if (args[0] != null) {
+                    Log.d("SOCKET: ERROR", "Lỗi trả về " + args[0]);
+                } else {
+                    JSONArray array = (JSONArray) args[1];
+                    for (int i = 0; i < array.length(); i++) {
+                        try {
+                            String memberId = (String) array.get(i);
+                            users.add(realm.where(User.class).equalTo("id", memberId).findFirst());
+                            ///cần return users
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
