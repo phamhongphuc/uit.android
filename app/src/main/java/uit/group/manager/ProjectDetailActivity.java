29f562c823dbeafd61556b1aadc34500fa933f30
Huyền <henrynguyen02081997@gmail.com>
2018-01-02 11:35:06 +0700

    Update project detail

--- app/src/main/java/uit/group/manager/ProjectDetailActivity.java
+++ app/src/main/java/uit/group/manager/ProjectDetailActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+//import object.Channel;
 import object.Project;
 import uit.group.manager.databinding.ActivityProjectDetailBinding;
+import view.adapter.ChannelRecyclerViewAdapter;
+import view.adapter.TaskRecyclerViewAdapter;
 import view.state.ProjectDetailState;
 
 public class ProjectDetailActivity extends AppCompatActivity {
@@ -26,6 +31,30 @@ public class ProjectDetailActivity extends AppCompatActivity {
         if(projectId != -1){
             state.project.set(Project.getProjectById(projectId));
         }
+    }
+
+    private void InitializeChannelsRecyclerView() {
+        ChannelRecyclerViewAdapter adapter = new ChannelRecyclerViewAdapter(state.project.get().getChannels());
+        RecyclerView recyclerView = findViewById(R.id.list_channels);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void InitializeTasksRecyclerView() {
+        TaskRecyclerViewAdapter adapter = new TaskRecyclerViewAdapter(state.project.get().getTasks());
+        RecyclerView recyclerView = findViewById(R.id.list_tasks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void InitializeMembersRecyclerView() {
+        ChannelRecyclerViewAdapter adapter = new ChannelRecyclerViewAdapter(state.project.get().getChannels());
+        RecyclerView recyclerView = findViewById(R.id.list_members);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
     }
 
     private void InitializeDataBinding() {
