32c2f3985e7cf48b5e3885afd1af2e74978eae85
Huyền <henrynguyen02081997@gmail.com>
2018-01-02 10:17:45 +0700

    Update xml

--- app/src/main/java/uit/group/manager/TaskEditActivity.java
+++ app/src/main/java/uit/group/manager/TaskEditActivity.java
@@ -3,11 +3,11 @@ package uit.group.manager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProjectEditActivity extends AppCompatActivity {
+public class TaskEditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_project_edit);
+        setContentView(R.layout.activity_task_edit);
     }
 }
