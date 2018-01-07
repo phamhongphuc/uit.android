9bea6dfb0eda6105bb7636548352aed350d544cb
Huyền <henrynguyen02081997@gmail.com>
2018-01-07 10:57:02 +0700

    Update Chat

--- app/src/main/java/uit/group/manager/ChatActivity.java
+++ app/src/main/java/uit/group/manager/ChatActivity.java
@@ -4,10 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class ChatActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
     }
+/*    private void InitializeMessagesRecyclerView() {
+        MessageRecyclerViewAdapter adapter = new MessageRecyclerViewAdapter();
+        RecyclerView recyclerView = findViewById(R.id.list_messages);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void InitializeDataBinding() {
+        ActivityProjectDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_chat);
+        binding.setSocket(_Socket.State.getInstance());
+
+        binding.setState(state);
+        binding.setGlobal(Global.getInstance());
+    }*/
 }
